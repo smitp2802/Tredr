deaf70cff014d5167f980fc33e26670f59aaa66a
smitp2802 <smitp2802@gmail.com>
2026-05-24 20:29:28 +0530

    Update main.py

--- strategy/main.py
+++ strategy/main.py
@@ -74,9 +74,7 @@ def fetch_data():
     return df
 
 exchange.load_markets()
-
 print("TOTAL SYMBOLS:", len(exchange.symbols))
-
 for s in exchange.symbols[:20]:
     if "BTC" in s:
         print(s)
